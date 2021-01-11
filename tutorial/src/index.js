c4b3e858091175b14fec0101613223c3ee972827
Ryan Hong <you@example.com>
2021-01-11 15:33:38 -0400

    first commit

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,3 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function 
+
+
+function Greeting(){
+    return <h4>this is ryan and this is my first component</h4>;
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
