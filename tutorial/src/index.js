5a8f5a4f3d70e25bf1b41bb8b55f86779c682532
Ryan Hong <you@example.com>
2021-01-11 16:38:30 -0400

    stateless functional component and nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+//! stateless fucntional component
+// always return JSX
 
+// function Greeting() {
+//   return (
+//     <div>
+//       <h4>this is ryan and this is my first component</h4>
+//     </div>
+//   );
+// }
 
-function Greeting(){
-    return <h4>this is ryan and this is my first component</h4>;
+// const alternative --> can get pretty messy
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "hello world")
+//   );
+// };
+
+//! Nested Components
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Person = () => <h2>john doe</h2>; // implicit return
+const Message = () => {
+  // explicit return
+  return <p>this is a message</p>;
+};
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
